3cd92fd8044d048554ca779ddc3a536bbfa67a18
Josh Cummings <josh.cummings@gmail.com>
2020-08-01 14:46:28 -0600

    Step Two: In-memory Users
    We can authenticate a REST API with an in-memory set of users. This
    works for only the simplest of APIs, but is certainly handy to get us
    started.

--- src/main/java/io/jzheaux/springsecurity/goals/GoalsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/goals/GoalsApplication.java
@@ -2,10 +2,22 @@ package io.jzheaux.springsecurity.goals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class GoalsApplication {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new InMemoryUserDetailsManager(
+				User.withUsername("user")
+					.password("{bcrypt}$2a$10$3njzOWhsz20aimcpMamJhOnX9Pb4Nk3toq8OO0swIy5EPZnb1YyGe")
+					.authorities("goal:read")
+					.build());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoalsApplication.class, args);
