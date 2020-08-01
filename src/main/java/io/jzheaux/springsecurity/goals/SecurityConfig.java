10e9cb9e87e85174410c1faedc0a9c96dfcdfbfe
Josh Cummings <josh.cummings@gmail.com>
2020-08-01 15:01:15 -0600

    Step Three: A Spring Data-based UserDetailsService
    A UserDetailsService is designed to be able to query any repository by
    username, and Spring Data repositories are a great fit.

--- src/main/java/io/jzheaux/springsecurity/goals/SecurityConfig.java
+++ src/main/java/io/jzheaux/springsecurity/goals/SecurityConfig.java
@@ -1,20 +1,8 @@
 package io.jzheaux.springsecurity.goals;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		return new InMemoryUserDetailsManager(
-				User.withUsername("user")
-						.password("{bcrypt}$2a$10$3njzOWhsz20aimcpMamJhOnX9Pb4Nk3toq8OO0swIy5EPZnb1YyGe")
-						.authorities("goal:read")
-						.build());
-	}
 }
--- src/main/java/io/jzheaux/springsecurity/goals/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/goals/UserRepositoryUserDetailsService.java
@@ -1,4 +1,57 @@
 package io.jzheaux.springsecurity.goals;
 
-public class UserRepositoryUserDetailsService {
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+	private final UserRepository users;
+
+	public UserRepositoryUserDetailsService(UserRepository users) {
+		this.users = users;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return this.users.findByUsername(username)
+				.map(UserBridge::new)
+				.orElseThrow(() -> new UsernameNotFoundException("Couldn't find user"));
+	}
+
+	private static class UserBridge extends User implements UserDetails {
+		UserBridge(User user) {
+			super(user);
+		}
+
+		@Override
+		public Collection<? extends GrantedAuthority> getAuthorities() {
+			Collection<GrantedAuthority> authorities = new ArrayList<>();
+			for (UserAuthority userAuthority : this.userAuthorities) {
+				authorities.add(new SimpleGrantedAuthority(userAuthority.authority));
+			}
+			return authorities;
+		}
+
+		@Override
+		public boolean isAccountNonExpired() {
+			return true;
+		}
+
+		@Override
+		public boolean isAccountNonLocked() {
+			return true;
+		}
+
+		@Override
+		public boolean isCredentialsNonExpired() {
+			return true;
+		}
+	}
 }
