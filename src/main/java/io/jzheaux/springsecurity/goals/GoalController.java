ae8d1221bcbb241d7ba0e52ea6b9a45a47993a8a
Josh Cummings <josh.cummings@gmail.com>
2020-08-08 17:22:26 -0600

    Step One: Allowing CORS at an Endpoint
    Adding the @CrossOrigin annotation to a Spring MVC mapping opens up
    calling that method to any Origin.
    
    Calling the `cors` method on the Spring Security DSL configures Spring
    Security to allow the preflight request without the user first being
    authenticated.

--- src/main/java/io/jzheaux/springsecurity/goals/GoalController.java
+++ src/main/java/io/jzheaux/springsecurity/goals/GoalController.java
@@ -4,6 +4,7 @@ import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ public class GoalController {
 		this.users = users;
 	}
 
+	@CrossOrigin
 	@GetMapping("/goals")
 	@PreAuthorize("hasAuthority('goal:read')")
 	@PostFilter("@post.filter(#root)")
--- src/main/java/io/jzheaux/springsecurity/goals/GoalsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/goals/GoalsApplication.java
@@ -3,10 +3,23 @@ package io.jzheaux.springsecurity.goals;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class GoalsApplication {
+public class GoalsApplication extends WebSecurityConfigurerAdapter {
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests(authz -> authz
+				.anyRequest().authenticated()
+			)
+			.httpBasic(withDefaults())
+			.cors(withDefaults());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoalsApplication.class, args);
