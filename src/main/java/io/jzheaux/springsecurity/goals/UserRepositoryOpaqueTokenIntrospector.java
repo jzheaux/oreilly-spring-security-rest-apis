09e66cdb58992ee34f25d986c4dee9b8f96f506d
Josh Cummings <josh.cummings@gmail.com>
2020-09-05 11:01:40 -0600

    Add Placeholders for OpaqueTokenIntrospector

--- src/main/java/io/jzheaux/springsecurity/goals/UserRepositoryOpaqueTokenIntrospector.java
+++ src/main/java/io/jzheaux/springsecurity/goals/UserRepositoryOpaqueTokenIntrospector.java
@@ -1,4 +1,73 @@
 package io.jzheaux.springsecurity.goals;
 
-public class UserRepositoryOpaqueTokenIntrospector {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class UserRepositoryOpaqueTokenIntrospector
+		implements OpaqueTokenIntrospector {
+
+	private final UserRepository users;
+	private final OpaqueTokenIntrospector introspector;
+
+	public UserRepositoryOpaqueTokenIntrospector(
+			UserRepository users, OpaqueTokenIntrospector introspector) {
+
+		this.users = users;
+		this.introspector = introspector;
+	}
+
+	@Override
+	public OAuth2AuthenticatedPrincipal introspect(String token) {
+		OAuth2AuthenticatedPrincipal principal = this.introspector.introspect(token);
+		User user = this.users.findByUsername(principal.getName())
+				.orElseThrow(() -> new UsernameNotFoundException("user not found"));
+		Collection<GrantedAuthority> authorities = user.getUserAuthorities().stream()
+				.map(userAuthority -> new SimpleGrantedAuthority(userAuthority.authority))
+				.collect(Collectors.toList());
+		Collection<GrantedAuthority> scopes = principal.getAuthorities().stream()
+				.map(scope -> new SimpleGrantedAuthority(scope.getAuthority().substring(6)))
+				.collect(Collectors.toList());
+		authorities.retainAll(scopes);
+
+		// add the virtual authority goal:share that's based on whether this
+		// authentication can goal:write authority as well as the user has a premium membership
+
+		return new UserOAuth2AuthenticatedPrincipal(user, principal.getAttributes(), authorities);
+	}
+
+	private static class UserOAuth2AuthenticatedPrincipal extends User
+			implements OAuth2AuthenticatedPrincipal {
+
+		private final Map<String, Object> attributes;
+		private final Collection<GrantedAuthority> authorities;
+
+		public UserOAuth2AuthenticatedPrincipal(
+				User user, Map<String, Object> attributes, Collection<GrantedAuthority> authorities) {
+			super(user);
+			this.attributes = attributes;
+			this.authorities = authorities;
+		}
+
+		@Override
+		public Map<String, Object> getAttributes() {
+			return this.attributes;
+		}
+
+		@Override
+		public Collection<? extends GrantedAuthority> getAuthorities() {
+			return this.authorities;
+		}
+
+		@Override
+		public String getName() {
+			return this.username;
+		}
+	}
 }
