b7ccd5ce58689671949915825377a01e0d8f62b2
Josh Cummings <josh.cummings@gmail.com>
2020-09-05 08:58:27 -0600

    Add UserService implementation

--- src/main/java/io/jzheaux/springsecurity/goals/UserService.java
+++ src/main/java/io/jzheaux/springsecurity/goals/UserService.java
@@ -1,9 +1,24 @@
 package io.jzheaux.springsecurity.goals;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import java.util.Optional;
 
+@Service
 public class UserService {
+	private final WebClient web;
+
+	public UserService(WebClient.Builder web) {
+		this.web = web.build();
+	}
+
 	public Optional<String> getFullName(String username) {
-		return Optional.empty();
+		String name = this.web.get()
+				.uri("/user/{username}/fullName", username)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+		return Optional.ofNullable(name);
 	}
 }
