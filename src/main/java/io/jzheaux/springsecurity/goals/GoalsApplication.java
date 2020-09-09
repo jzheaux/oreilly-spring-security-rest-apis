3849e002fd3b235cdbf6c61cc36fb0b48929b336
Josh Cummings <josh.cummings@gmail.com>
2020-09-09 06:26:09 -0600

    Step Three: Global CORS Settings
    You can centralize our baseline CORS rules by publishing a
    WebMvcConfigurer @Bean that overrides the addCorsMapping method.
    
    Keep in mind that by default Spring MVC will allow any idempotent
    method. You can tighten that up by only allowing HEAD by default
    and then progressively adding exceptions here or with @CorsMapping
    annotations.

--- src/main/java/io/jzheaux/springsecurity/goals/GoalsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/goals/GoalsApplication.java
@@ -2,9 +2,13 @@ package io.jzheaux.springsecurity.goals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,6 +23,19 @@ public class GoalsApplication extends WebSecurityConfigurerAdapter {
 			)
 			.httpBasic(withDefaults())
 			.cors(withDefaults());
+	}
+
+	@Bean
+	WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4000")
+						.allowedMethods("HEAD")
+						.allowedHeaders("Authorization");
+			}
+		};
 	}
 
 	public static void main(String[] args) {
