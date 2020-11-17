6e0889cb4ddd2d332cb1ce43039648eb23f8045e
Josh Cummings <josh.cummings@gmail.com>
2020-11-16 18:45:22 -0700

    Adjusted CsrfHeaderAdvice

--- src/main/java/io/jzheaux/springsecurity/goals/CsrfHeaderAdvice.java
+++ src/main/java/io/jzheaux/springsecurity/goals/CsrfHeaderAdvice.java
@@ -1,5 +1,6 @@
 package io.jzheaux.springsecurity.goals;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.web.csrf.CsrfToken;
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class CsrfHeaderAdvice {
 	@ModelAttribute("csrf")
-	public CsrfToken token(CsrfToken token, HttpServletResponse response) {
-		response.setHeader(token.getHeaderName(), token.getToken());
+	public CsrfToken token(HttpServletRequest request, HttpServletResponse response) {
+		CsrfToken token = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+		if (token != null) {
+			response.setHeader(token.getHeaderName(), token.getToken());
+		}
 		return token;
 	}
 }
