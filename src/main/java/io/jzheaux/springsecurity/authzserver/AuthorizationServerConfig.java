2c0f8e6bcb8018e072be4a34b29fbec7942d1c80
Josh Cummings <josh.cummings@gmail.com>
2022-03-31 22:20:29 -0600

    Update to Spring Authorization Server 0.2.3

--- src/main/java/io/jzheaux/springsecurity/authzserver/AuthorizationServerConfig.java
+++ src/main/java/io/jzheaux/springsecurity/authzserver/AuthorizationServerConfig.java
@@ -1,23 +1,36 @@
 package io.jzheaux.springsecurity.authzserver;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerSecurity;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.keys.KeyManager;
-import org.springframework.security.crypto.keys.StaticKeyGeneratingKeyManager;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
@@ -25,17 +38,30 @@ import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static org.springframework.beans.factory.config.BeanDefinition.ROLE_INFRASTRUCTURE;
+
 @Configuration
 public class AuthorizationServerConfig {
 
 	@Bean
 	@Order(1)
-	SecurityFilterChain oauth2Endpoints(HttpSecurity http) throws Exception {
+	public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+		return http.formLogin(Customizer.withDefaults()).build();
+	}
+
+	@Bean
+	@Order(2)
+	SecurityFilterChain appEndpoints(HttpSecurity http) throws Exception {
+		// @formatter:off
 		http
 			.addFilterBefore(new ForwardedHeaderFilter(), LogoutFilter.class)
-			.cors(Customizer.withDefaults());
-		OAuth2AuthorizationServerSecurity.applyDefaultConfiguration(http);
+			.authorizeRequests((authz) -> authz.anyRequest().authenticated())
+			.formLogin(Customizer.withDefaults())
+			.oauth2ResourceServer((oauth2) -> oauth2
+					.jwt(Customizer.withDefaults()));
 		return http.build();
+		// @formatter:on
 	}
 
 	@Bean
@@ -56,21 +82,34 @@ public class AuthorizationServerConfig {
 		RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
 				.clientId("goals-client")
 				.clientSecret("secret")
-				.clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
 				.redirectUri("http://localhost:8081/bearer.html")
 				.scope("goal:read")
 				.scope("goal:write")
 				.scope("user:read")
-				.clientSettings((settings) -> settings.requireUserConsent(true))
+				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
 				.build();
 		return new InMemoryRegisteredClientRepository(registeredClient);
 	}
 	// @formatter:on
 
 	@Bean
-	public KeyManager keyManager() {
-		return new StaticKeyGeneratingKeyManager();
+	public JWKSource<SecurityContext> jwkSource(KeyPair keyPair) {
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		// @formatter:off
+		RSAKey rsaKey = new RSAKey.Builder(publicKey)
+				.privateKey(privateKey)
+				.keyID(UUID.randomUUID().toString())
+				.build();
+		JWKSet jwkSet = new JWKSet(rsaKey);
+		return new ImmutableJWKSet<>(jwkSet);
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder(KeyPair keyPair) {
+		return NimbusJwtDecoder.withPublicKey((RSAPublicKey) keyPair.getPublic()).build();
 	}
 
 	// @formatter:off
@@ -101,16 +140,22 @@ public class AuthorizationServerConfig {
 	// @formatter:on
 
 	@Bean
-	@Order(2)
-	SecurityFilterChain appEndpoints(HttpSecurity http) throws Exception {
-		// @formatter:off
-		http
-			.addFilterBefore(new ForwardedHeaderFilter(), LogoutFilter.class)
-			.authorizeRequests((authz) -> authz.anyRequest().authenticated())
-			.formLogin(Customizer.withDefaults())
-			.oauth2ResourceServer((oauth2) -> oauth2
-				.jwt(Customizer.withDefaults()));
-		return http.build();
-		// @formatter:on
+	public ProviderSettings providerSettings() {
+		return ProviderSettings.builder().issuer("http://idp:8083").build();
+	}
+
+	@Bean
+	@Role(ROLE_INFRASTRUCTURE)
+	KeyPair generateRsaKey() {
+		KeyPair keyPair;
+		try {
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			keyPair = keyPairGenerator.generateKeyPair();
+		}
+		catch (Exception ex) {
+			throw new IllegalStateException(ex);
+		}
+		return keyPair;
 	}
 }
